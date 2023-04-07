362f04334911551abae927eb031b6dcb92f786b3
citec-47 <mauricendonyi40@gmail.com>
2023-04-06 18:37:46 -0700

    status-update

--- add-remove-Items.test.js
+++ add-remove-Items.test.js
@@ -7,12 +7,12 @@ describe('Adding Items and Removing Testing', () => {
   <div id='containerItems'></div>
   `;
 
-  test('Items To The list', () => {
+  test('Items to be editid ', () => {
     addListItems('Football', false, 0);
     const localListItems = JSON.parse(localStorage.getItem('listStorage'));
     expect(localListItems.length).toBe(1);
   });
-  test('Remove item from the list', () => {
+  test('Updating status items completed', () => {
     addListItems('work', false, 1);
     const localListItems = JSON.parse(localStorage.getItem('listStorage'));
     expect(localListItems.length).toBe(2);
@@ -30,24 +30,6 @@ describe('Testing  update if it is functional', () => {
     <button id='addinBtn'></button>
     <div id='containerItems'></div>
     `;
-
-  test('Can all completed updated Items bet true', () => {
-    const localListItems = JSON.parse(localStorage.getItem('listStorage'));
-    localListItems[0].completed = true;
-    localStorage.setItem('listStorage', JSON.stringify(localListItems));
-    renderingListItems();
-    const itemsToDo = document.getElementById('containerItems');
-    expect(itemsToDo.children[0].children[0].checked).toBe(true);
-  });
-
-  test('Can all completed updated Items bet false', () => {
-    const localListItems = JSON.parse(localStorage.getItem('listStorage'));
-    localListItems[0].completed = false;
-    localStorage.setItem('listStorage', JSON.stringify(localListItems));
-    renderingListItems();
-    const itemsToDo = document.getElementById('containerItems');
-    expect(itemsToDo.children[0].children[0].checked).toBe(false);
-  });
   test('Can  remove all completed be true', () => {
     const localListItems = JSON.parse(localStorage.getItem('listStorage'));
     localListItems.pop();
